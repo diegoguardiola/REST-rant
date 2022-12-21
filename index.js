b5fd1af47649352d2dbd2173472228fb97e8aee7
Diego I. Guardiola <diegoguardiola01@gmail.com>
2022-12-20 19:51:11 -0800

    Adding 404 page view

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
   
 //404 placed at BOTTOM
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    res.render('error404')
 })
 
 //gets port info from ENV file
