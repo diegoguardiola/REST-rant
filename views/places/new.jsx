db10a80ee57ef3b055b6cc12bd3cb8f36c18e032
Diego I. Guardiola <diegoguardiola01@gmail.com>
2023-01-19 01:33:10 -0500

    Validation Error For New

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -1,39 +1,73 @@
 const React = require('react')
 const Def = require('../default')
 
-function newForm () {
+function newForm (data) {
+    let message =''
+    if (data.message) {
+      message = (
+        <h4 className="alert-danger">
+          {data.message}
+        </h4>
+      )
+    }
     return (
         <Def>
             <main>
                 <h1>Add a New Place</h1>
+                {message}
                 <form method="POST" action="/places">
                     <div className='row'>
-                        <div className="form-group">
+                        <div className="form-group col-sm-4">
                             <label htmlFor="name">Place Name</label>
-                            <input className="form-control" id="name" name="name" required />
+                            <input 
+                                className="form-control" 
+                                id="name"
+                                 name="name" 
+                                 required />
                         </div>
-                        <div className="form-group">
+                        <div className="form-group col-sm-4">
                             <label htmlFor="pic">Place Picture</label>
-                            <input className="form-control" id="pic" name="pic" />
+                            <input 
+                                className="form-control" 
+                                id="pic" 
+                                name="pic" />
                         </div>
-                        <div className="form-group">
+                        <div className="form-group col-sm-4">
                             <label htmlFor="city">City</label>
-                            <input className="form-control" id="city" name="city" />
+                            <input 
+                                className="form-control" 
+                                id="city" 
+                                name="city" />
                         </div>
-                        <div className="form-group">
+                        <div className="form-group col-sm-4">
                             <label htmlFor="state">State</label>
-                            <input className="form-control" id="state" name="state" />
+                            <input 
+                                className="form-control" 
+                                id="state" 
+                                name="state" />
                         </div>
-                        <div className="form-group">
+                        <div className="form-group col-sm-4">
                             <label htmlFor="cuisines">Cuisines</label>
-                            <input className="form-control" id="cuisines" name="cuisines" required />
+                            <input 
+                                className="form-control" 
+                                id="cuisines" 
+                                name="cuisines" 
+                                required />
                         </div>
-                        <div className="form-group">
+                        <div className="form-group col-sm-4">
                             <label for="founded">Founded Year</label>
-                            <input className="form-control" id="founded" name="founded" />
+                            <input 
+                                type='Number'
+                                className="form-control" 
+                                id="founded" 
+                                name="founded" 
+                                value={new Date().getFullYear()} />
                         </div>
                     </div>
-                    <input className="btn btn-primary" type="submit" value="Add Place" />
+                    <input 
+                        className="btn btn-primary" 
+                        type="submit" 
+                        value="Add Place" />
                  </form>
 
             </main>
