561d86009be8541677e177e4df55015738e0c47a
Diego I. Guardiola <diegoguardiola01@gmail.com>
2023-01-18 22:31:23 -0500

    creating schema 2

--- controllers/places.js
+++ controllers/places.js
@@ -1,68 +1,69 @@
 const router = require('express').Router()
-const e = require('express')
-const places = require('../models/places.js')
+const db = require('../models')
 
+
+//PLACES INDEX
 router.get('/', (req, res) => {
-    res.render('places/index', { places })
+    db.Place.find()
+    .then((places) => {
+      res.render('places/index', { places })
+    })
+    .catch(err => {
+      console.log(err) 
+      res.render('error404')
+    })
 })
 
-//GET new
+//POST
+router.post('/', (req, res) => {
+  db.Place.create(req.body)
+  .then(() => {
+      res.redirect('/places')
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
+})
+
+
+//NEW
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
-//show
+//SHOW PLACE
 router.get('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    res.render('places/show', {place:places[id]})
-  }
+  db.Place.findById(req.params.id)
+  .then(place => {
+      res.render('places/show', { place })
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
 })
 
-
-
-router.post('/', (req, res) => {
-  if (!req.body.pic) {
-    // Default image if one is not provided
-    req.body.pic = 'http://placekitten.com/400/400'
-  }
-  if (!req.body.city) {
-    req.body.city = 'Anytown'
-  }
-  if (!req.body.state) {
-    req.body.state = 'USA'
-  }
-  places.push(req.body)
-  res.redirect('/places')
-})  
-
-
-
-// GET places
-router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/h-thai-m-l.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/cafe.jpeg'
-      }]
-    res.render('places/index', {places})
+router.put('/:id', (req, res) => {
+  res.send('PUT /places/:id stub')
 })
-  
+
+//DELETE PLACE
+router.delete('/:id', (req, res) => {
+  res.send('DELETE /places/:id stub')
+})
+
+//EDIT PLACE
+router.get('/:id/edit', (req, res) => {
+  res.send('GET edit form stub')
+})
+
+router.post('/:id/rant', (req, res) => {
+  res.send('GET /places/:id/rant stub')
+})
+
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('GET /places/:id/rant/:rantId stub')
+})
 
 module.exports = router
-
-
